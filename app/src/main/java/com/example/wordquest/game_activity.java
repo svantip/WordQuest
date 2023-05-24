fae1e56d23f49ba96fbd84803dd2248936231f0e
svantip <svan.tipuric6@gmail.com>
2023-05-24 13:02:11 +0200

    third commit

--- app/src/main/java/com/example/wordquest/game_activity.java
+++ app/src/main/java/com/example/wordquest/game_activity.java
@@ -2,25 +2,23 @@ package com.example.wordquest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-
-import com.google.android.material.textfield.TextInputEditText;
 
 import java.util.Random;
 
 public class game_activity extends AppCompatActivity {
-
+    AlertDialog.Builder builder;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+        //Making the alert for displaying msg when user input is empty
+        builder = new AlertDialog.Builder(this);
         //Generate random word for testing -change later to display from database-
         String[] words = {"apple", "banana", "carrot", "dragon", "elephant", "flower", "guitar", "house", "island", "jacket"};
         Random random = new Random();
@@ -40,7 +38,11 @@ public class game_activity extends AppCompatActivity {
 
         TextView alreadyGuessedLettersView = (TextView) findViewById(R.id.alreadyGuessedLetters);
         alreadyGuessedLettersView.setLetterSpacing(0.2f);
+        TextView alreadyGuessedWordsView = (TextView) findViewById(R.id.alreadyGuessedWords);
         EditText txtBoxGuess = (EditText) findViewById(R.id.txtBoxGuess);
+        TextView txtBoxLives = findViewById(R.id.txtBoxLives);
+        
+
         //When button send on keyboard is pressed
         txtBoxGuess.setOnEditorActionListener(new TextView.OnEditorActionListener() {
             @Override
@@ -48,12 +50,18 @@ public class game_activity extends AppCompatActivity {
                 if (actionId == EditorInfo.IME_ACTION_SEND) {
                     //Get the text from the EditText
                     String userInput = txtBoxGuess.getText().toString().toLowerCase();
+                    txtBoxGuess.setText("");
+                    //Get hiddenWord
                     String hiddenWord = wordToBeGuessed.getText().toString();
+                    //Get alreadyGuessedWords and alreadyGuessedLetters
                     String alreadyGuessedLetters = alreadyGuessedLettersView.getText().toString();
+                    String alreadyGuessedWords = alreadyGuessedWordsView.getText().toString();
+                    //Get lives
+                    int lives = Integer.valueOf(txtBoxLives.getText().toString()); //String to integer
                     //Do something with text
                     if(userInput.length() == 1) {
                         //Input is a char
-                        alreadyGuessedLetters += userInput;
+                        alreadyGuessedLetters += userInput; //Displaying already guessed letters
                         alreadyGuessedLettersView.setText(alreadyGuessedLetters);
                         if(word.contains(userInput)) {
                             //Char matches a char inside word
@@ -66,18 +74,38 @@ public class game_activity extends AppCompatActivity {
                                     hiddenWord = String.valueOf(hiddenWordArray); //Switching it back to string
                                 }
                             }
+                            //Setting txtView text to hiddenWord
                             wordToBeGuessed.setText(hiddenWord);
                         }
                         else{
                             //Char doesn't match a char inside word
-
+                            lives--; //Lives -1
+                            String livesString = Integer.toString(lives);//Integer to string
+                            txtBoxLives.setText(livesString);//Displaying lives in txtView
+                            if(lives == 0){
+                                //Loser screen
+                            }
                         }
                     }else if(userInput.length() == 0){
                         //Input is empty
-
+                        builder.setMessage("You need to write something into input") .setTitle(getTitle());//Set alerts title and text
+                        builder.show();//Show alert
                     }else{
                         //Input is a string
+                        if(userInput.equals(word)){
+                            //Winner screen
 
+                        }
+                        else{
+                            alreadyGuessedWords+=userInput+" ";
+                            alreadyGuessedWordsView.setText(alreadyGuessedWords);
+                            lives--; //Lives -1
+                            String livesString = Integer.toString(lives);//Integer to string
+                            txtBoxLives.setText(livesString);//Displaying lives in txtView
+                            if(lives == 0){
+                                //Loser screen
+                            }
+                        }
                     }
                     return true;
                 }
