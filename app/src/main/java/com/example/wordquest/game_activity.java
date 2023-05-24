d7e1ffa836153ee36d8661d3686551695cf8f780
matijatomc <tomc.matija@gmail.com>
2023-05-25 01:18:46 +0200

    sixth commit

--- app/src/main/java/com/example/wordquest/game_activity.java
+++ app/src/main/java/com/example/wordquest/game_activity.java
@@ -142,7 +142,6 @@ public class game_activity extends AppCompatActivity {
                         Intent intent = new Intent(game_activity.this, WiningScreen.class);
                         startActivity(intent);
                     }
-
                     return true;
                 }
                 return false;
